b2e68f99d2f867f013ffef052de88ce3325a9813
SungSeokMin <jkl154527@gmail.com>
2021-01-12 21:21:20 +0900

    clone coding

---
+++ src/components/Subscribers.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addSubscriber } from '../redux/subscribers/actions';
+
+const Subscribers = ({ count, addSubscriber }) => {
+  return (
+    <div>
+      <h2>구독자 수: {count}</h2>
+      <button onClick={() => addSubscriber()}>구독하기!</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addSubscriber: () => dispatch(addSubscriber()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Subscribers);
